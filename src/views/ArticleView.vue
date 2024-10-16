f7d7e0ca5fdcb69e95c1fdc7fc01f70a4269966e
tokolight <tokolight.id@gmail.com>
2024-10-16 23:08:58 +0800

    Update ArticleView.vue

--- src/views/ArticleView.vue
+++ src/views/ArticleView.vue
@@ -38,7 +38,7 @@ export default {
     methods: {
         async getDetails() {
             const id = this.route.params.id;
-            axios.get('https://64a38c9cc3b509573b564183.mockapi.io/api/blog/all/' + id)
+            axios.get('https://670fd6ada85f4164ef2c23b4.mockapi.io/api/blog/all/' + id)
                 .then(response => {
                     this.title = response.data.title;
                     this.image = response.data.image;
