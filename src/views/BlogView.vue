21fedc008f89ad3c5fe9534dcea969704fc9f0b3
tokolight <tokolight.id@gmail.com>
2024-10-17 19:31:54 +0800

    Update BlogView.vue

--- src/views/BlogView.vue
+++ src/views/BlogView.vue
@@ -64,7 +64,7 @@ export default {
   },
   methods: {
     async getArticles() {
-      axios.get('https://670fd6ada85f4164ef2c23b4.mockapi.io/api/blog/all/')
+      axios.get('https://670fd6ada85f4164ef2c23b4.mockapi.io/api/blog/All')
         .then(response => {
           this.articles = response.data;
         })
