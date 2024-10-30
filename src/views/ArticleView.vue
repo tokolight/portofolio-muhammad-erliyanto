898f7b4bc809f23f3bd4548b016994983d7f2875
tokolight <tokolight.id@gmail.com>
2024-10-30 20:02:09 +0800

    Update ArticleView.vue

--- src/views/ArticleView.vue
+++ src/views/ArticleView.vue
@@ -24,9 +24,7 @@
       <div class="w-full md:w-1/3 h-fit p-8 md:sticky md:top-24">
         <!-- Sidebar -->
         <div class="flex flex-col text-left">
-          <div class="bg-clip-text bg-gradient-to-r from-slate-100 to-amber-300 text-transparent">Let's share experiences,
-            stories, and knowledge together.
-          </div>
+          <div class="bg-clip-text bg-gradient-to-r from-slate-100 to-amber-300 text-transparent">Let's share experiences, stories, and knowledge together.</div>
           <div class="h-[1px] mt-7 mb-7 w-20 bg-amber-200 aos-init aos-animate mr-2"></div>
           <div class="hidden md:block">
             <div class="text-white text-md font-semibold">Topics</div>
@@ -111,7 +109,10 @@ export default {
         }
       ]
     }
-  }
+  },
+  mounted() {
+    // Jika perlu, bisa menambahkan logika di sini
+  },
 }
 </script>
 
