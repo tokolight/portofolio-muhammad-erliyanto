6f0e1042d38d74447c8edace74568858a6ba68fd
tokolight <tokolight.id@gmail.com>
2024-10-30 23:04:11 +0800

    Update PortfolioView.vue

--- src/views/PortfolioView.vue
+++ src/views/PortfolioView.vue
@@ -64,14 +64,14 @@ export default {
 }
 </script>
 <template>
-  <div class="px-5 py-5 md:px-12 md:py-10 text-left text-[#00ffff]-50 mx-3">
+  <div class="px-5 py-5 md:px-12 md:py-10 text-left text-[#00ffff] mx-3">
     <article data-page="about">
       <header>
         <div
           class="text-2xl font-bold text-white mb-10 fadein-bot title-section flex items-center justify-center flex-col">
-          <!-- <div class="h-[1px] w-10 bg-[#00ffff]-200 md:w-20 aos-init aos-animate"></div> -->
+          <!-- <div class="h-[1px] w-10 bg-[#00ffff] md:w-20 aos-init aos-animate"></div> -->
           <h4>Past Project Experience</h4>
-          <h4 class="text-base font-normal text-transparent bg-clip-text bg-gradient-to-r from-slate-100 to-[#00ffff]-300">
+          <h4 class="text-base font-normal text-transparent bg-clip-text bg-gradient-to-r from-slate-100 to-[#00ffff]">
             Explore the projects I've worked on so far</h4>
         </div>
       </header>
@@ -80,7 +80,7 @@ export default {
           <div class="grid grid-cols-1 gap-4 pb-32 md:grid-cols-3 md:gap-3 xl:grid-cols-3 xl:gap-3 2xl:gap-5 fade-zoom-in">
             <div v-for="item in items" :key="item.id">
               <div
-                class="item-card flex flex-col items-center gap-2 rounded bg-[#1e1e1f] hover:bg-[#282828] border border-[#383838] rounded-xl text-[#00ffff]-50 md:gap-3 px-5 py-5 lg:px-5 ">
+                class="item-card flex flex-col items-center gap-2 rounded bg-[#1e1e1f] hover:bg-[#282828] border border-[#383838] rounded-xl text-[#00ffff] md:gap-3 px-5 py-5 lg:px-5 ">
                 <div class="flex h-12 w-12 items-center justify-center p-0 h-full w-full lg:p-0 zoom-in">
                   <img alt="HTML" loading="lazy" decoding="async" data-nimg="1" class="drop-shadow-xl rounded rounded-xl"
                     :src="'/img/portfolio-' + item.imageUrl + '.png'">
@@ -90,7 +90,7 @@ export default {
                   </div>
                   <div class="w-full text-left text-[10px] text-[#c1c1c1] md:text-xs lg:text-sm">
                     {{ item.status }}</div>
-                  <div class="w-full mt-4 text-normal text-sm text-left text-[#00ffff]-200">
+                  <div class="w-full mt-4 text-normal text-sm text-left text-[#00ffff]">
                     {{ item.tech }}
                   </div>
                   <div class="w-full flex justify-end">
